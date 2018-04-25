3d68920f0afe1b7518ffaf4d928384743ae56027
Cressence <cressencey@gmail.com>
2018-04-25 19:11:36 +0100

    SPlash screen setup

--- App.js
+++ App.js
@@ -12,54 +12,23 @@ import {
   View
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { Router, Scene } from 'react-native-router-flux';
+import Splash from './src/Splash/Splash';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
 class App extends Component {
-    componentDidMount() {
-        // setTimeout(() => Actions.welcome(), 3000);
-      }
       
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Router 
+      navigationBarStyle={{ backgroundColor: '#012C3D' }}
+      >
+        <Scene key='root'>
+        <Scene key="splash" component={Splash}  initial />
+          </Scene>
+      </Router>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 export default App;
